565d1dfa99f34d405e781e8d53d877b305e8726f
Heydar <h_h_z_n@yahoo.com>
2024-09-22 00:03:03 +0330

    refactor Nav component

--- React-TS/src/components/Nav.tsx
+++ React-TS/src/components/Nav.tsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
 export default function Nav() {
   return (
-    <nav className="bg-blue-200 py-2 flex items-center justify-between text-white child:rounded child:border child:border-blue-800 child:bg-blue-700 child:shadow-blue-800 child:px-4 child:py-2 px-4">
-      <NavLink to="/">Home</NavLink>
+    <nav className="bg-blue-200 py-2 flex items-center justify-evenly text-white child:rounded child:border child:border-blue-800 child:bg-blue-700 child:shadow-blue-800 child:px-4 child:py-2 px-4">
       <NavLink to="/nasa">NasaList</NavLink>
-      <NavLink to="/todo">TodoList</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/todo">TodoApp</NavLink>
     </nav>
   );
 }
