dd624b05b3fd16b35d7926e4354c259ad5e5f10e
Heydar <h_h_z_n@yahoo.com>
2024-09-22 00:02:47 +0330

    add details of pages in home page

--- React-TS/src/App.tsx
+++ React-TS/src/App.tsx
@@ -7,6 +7,20 @@ function App() {
   return (
     <>
       <Nav />
+      <div className="flex items-start gap-14 mt-6 px-16 w-full">
+        <div className="flex items-center gap-4 justify-center w-full">
+          <h3 className="font-extrabold text-xl bg-blue-50 px-4 py-2 rounded text-blue-800">
+            Nasa
+          </h3>
+          <p className="text-sm text-blue-800">get data from nasa api</p>
+        </div>
+        <div className="flex items-center gap-4 justify-center w-full">
+          <h3 className="font-extrabold text-xl bg-blue-50 px-4 py-2 rounded text-blue-800">
+            TodoApp
+          </h3>
+          <p className="text-sm text-blue-800">create TodoApp with MockAPI</p>
+        </div>
+      </div>
       {router}
     </>
   );
